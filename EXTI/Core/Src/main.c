f3357524e8c6be2f646dd4a82e69c66ac2300102
Rodrigo Celmer <rodrigo@raks.com.br>
2020-03-04 16:13:34 -0300

    Add frequency management with EXTI

--- EXTI/Core/Src/main.c
+++ EXTI/Core/Src/main.c
@@ -15,40 +15,23 @@
 #define BLUE_LED_OFF		(GPIOB->ODR &= ~GPIO_ODR_OD1_Msk)
 #define TOGGLE_BLUE_LED		(GPIOB->ODR ^= GPIO_IDR_ID1_Msk)
 
+//--- GENERAL
+#define UP		1
+#define DOWN	0
+
 //--- Global Variables
 int user_bt_count	=0,	//counter to debounce the button
 	count			=0,	//counter to toggle the red led
 	flag_EXTI		=0,	//flag for EXTI interrupt
-	flag_TIM6		=0;	//flag for TIM6 interrupt
+	flag_TIM6		=0,	//flag for TIM6 interrupt
+	flag_frequency	=1;	//flag to indicate if we are increasing or decreasing the frequency
 
-void TIM6_DAC_IRQHandler(void)		//interrupts every 1 ms
+void TIM6_DAC_IRQHandler(void)
 {
 	if(TIM6->SR & TIM_SR_UIF)
 	{
+		flag_TIM6 = 1;				//sets TIM6 flag
 		TIM6->SR &= ~TIM_SR_UIF;	//Clear timer interrupt flag
-		count++;
-		if(count==100)
-		{
-			TOGGLE_RED_LED;			//toggle the red led every 100 ms
-			count=0;
-		}
-		if(flag_EXTI)										//if there was an EXTI int
-		{
-			user_bt_count++;								//counts up to 50 (50 ms)
-			if(user_bt_count==50)
-			{
-				TOGGLE_GREEN_LED;							//turns on/off the green led
-
-			}
-			if((user_bt_count>=50) && (!USR_BT_PRESS))		//if the led has been toggled and we released the button
-			{
-				flag_EXTI=0;								//clears flag
-			}
-		}
-		else
-		{
-			user_bt_count=0;
-		}
 	}
 }
 
@@ -56,7 +39,7 @@ void EXTI4_15_IRQHandler (void)
 {
 	if(EXTI->PR & EXTI_PR_PIF10)	//if there's EXTI interrupt
 	{
-		flag_EXTI=1;				//sets aux flag for toggling the green led
+		flag_EXTI=1;				//sets EXTI flag
 		EXTI->PR |= EXTI_PR_PIF10;	//clears the EXTI flag
 	}
 }
@@ -93,8 +76,8 @@ int main (void)
 	RCC->APB1ENR 	|= RCC_APB1ENR_TIM6EN;
 
 	TIM6->DIER 		|= TIM_DIER_UIE;	//Enable interrupt
-	TIM6->PSC = 209;					//timer prescaler
-	TIM6->ARR = 9;						//counter counts up to (interrupt every 1 ms)
+	TIM6->PSC = 20;						//timer prescaler
+	TIM6->ARR = 209;					//counter counts up to
 	TIM6->EGR 		|= TIM_EGR_UG;		//Update registers values
 	//Enables IRQ and IRQ priority
 	NVIC_EnableIRQ(TIM6_DAC_IRQn);
@@ -109,7 +92,56 @@ int main (void)
 
 	while(1)
 	{
+		if(flag_TIM6)				//if there was a TIM6 interrupt
+		{
+			flag_TIM6 = 0;			//clears TIM6 flag
+			count++;
+			if(count==100)
+			{
+				TOGGLE_RED_LED;		//toggle the red led every 100 ms
+				count=0;
+			}
+		}//flag_TIM6
 
+		if(flag_EXTI)									//if there was an EXTI int
+		{
+			user_bt_count++;							//counts up to 50 for debouncing
+			if(user_bt_count==50)
+			{
+				if((TIM6->ARR > 19)&& flag_frequency == UP)
+				{
+					TIM6->ARR = TIM6->ARR - 10;
+					TIM6->EGR |= TIM_EGR_UG;		//Update registers values
+					if(TIM6->ARR <= 19)
+					{
+						flag_frequency = DOWN;
+						GREEN_LED_ON;					//Green led indicates we're going to decrease the frequency
+						BLUE_LED_OFF;
+					}
+				}
+				else{
+					if((TIM6->ARR <= 199) && flag_frequency == DOWN)
+					{
+						TIM6->ARR = TIM6->ARR + 10;
+						TIM6->EGR |= TIM_EGR_UG;		//Update registers values
+						if(TIM6->ARR >= 209)
+						{
+							flag_frequency = UP;
+							GREEN_LED_OFF;
+							BLUE_LED_ON;					//Blue led indicates we're going to increase the frequency
+						}
+					}
+				}
+			}
+			if((user_bt_count>=50) && (!USR_BT_PRESS))	//if the led has been toggled and we released the button
+			{
+				flag_EXTI=0;							//clears flag
+			}
+		}//flag_EXTI
+		else
+		{
+			user_bt_count=0;
+		}//flag_EXTI
 	}
 	return 0;
 }
