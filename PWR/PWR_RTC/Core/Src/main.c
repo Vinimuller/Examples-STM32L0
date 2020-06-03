eb88eaeba792aab479352ec8a112e97492558e08
Rodrigo Celmer <rodrigocelmer@hotmail.com>
2020-06-03 10:49:55 -0300

    ADD RTC init

--- PWR/PWR_RTC/Core/Src/main.c
+++ PWR/PWR_RTC/Core/Src/main.c
@@ -31,26 +31,33 @@ int main(void)
 
 			if((user_bt_count >= BUTTON_DEBOUNCE) && (GPIOA->IDR & GPIO_IDR_ID10_Msk))
 			{
-				//while(!(GPIOA->IDR & GPIO_IDR_ID10_Msk));	//holds here (for wake from stop mode)
 				GPIOA->ODR &= ~GPIO_ODR_OD7_Msk;
-
-
 				EXTI->PR |= EXTI_PR_PIF10;
-				SCB->SCR = SCB_SCR_SLEEPDEEP_Msk; // low-power mode = stop mode
-				PWR->CR &= ~PWR_CR_PDDS;		//making sure we're entering stop mode
-				PWR->CR |= PWR_CR_CWUF;		//clears WUF after 2 system clock cycles
 
-				PWR->CR |= 	PWR_CR_LPSDSR	|	//voltage regulator in low-power mode
-							PWR_CR_ULP		;	//ultra low power mode enable
+				SCB->SCR = SCB_SCR_SLEEPDEEP_Msk; 	// low-power mode = stop mode
+				PWR->CR &= ~PWR_CR_PDDS;			//making sure we're entering stop mode
+				PWR->CR |= PWR_CR_CWUF;				//clears WUF after 2 system clock cycles
+
+				PWR->CR |= 	PWR_CR_LPSDSR	|		//voltage regulator in low-power mode
+							PWR_CR_ULP		;		//ultra low power mode enable
 				//LPSDSR bit must be set before the LPRUN bit is set
-				PWR->CR |=	PWR_CR_LPRUN;		//voltage regulator in low-power mode
+				PWR->CR |=	PWR_CR_LPRUN;			//voltage regulator in low-power mode
 
 				//entering stop mode procedure
 //				DBGMCU->CR |= DBGMCU_CR_DBG_STOP;	//this bit needs to be set if you're going to debug this code
 
-				__WFI();
+				PWR->CR |=	PWR_CR_DBP;				//enable write access to the RTC registers
+				// --- Unlocking RTC's write protection
+				RTC->WPR = 0xCA;
+				RTC->WPR = 0x53;
+				// --- RTC's unlocked
+				RTC->CR |= RTC_CR_WUTE;				//enables RTC - starts counting
+
+				__WFI();	//stop mode
 
 				while(!(GPIOA->IDR & GPIO_IDR_ID10_Msk));	//holds here (for wake from stop mode)
+
+				RTC->ISR &= ~RTC_ISR_WUTF;			//clears RTC wakeup flag
 
 				MCU_Init();
 
@@ -100,17 +107,22 @@ void MCU_Init(void)
 	/*							   *
 	 *  --- RTC INITIALIZATION --- *
 	 *							   */
-	RCC->CSR	|=	RCC_CSR_RTCSEL_LSI;	//sets LSI as RTC clock source (37 kHz)
-	RCC->CSR	|=	RCC_CSR_RTCEN;		//enables the RTC clock
+	RCC->CSR	|=	RCC_CSR_RTCSEL_LSI;		//sets LSI as RTC clock source (37 kHz)
+	RCC->CSR	|=	RCC_CSR_RTCEN;			//enables the RTC clock
 
-	PWR->CR |=	PWR_CR_DBP;				//enable write access to the RTC
+	PWR->CR |=	PWR_CR_DBP;					//enable write access to the RTC registers
 	// --- Unlocking RTC's write protection
 	RTC->WPR = 0xCA;
 	RTC->WPR = 0x53;
 	// --- RTC's unlocked
-	RTC->CR &= ~RTC_CR_WUTE;			//disables the wakeup timer
-	while(!(RTC->ISR & RTC_ISR_WUTWF));	//polling WUTWF until it is set
-	RTC->WUTR = 30;						//wakeup timer set to 30 seconds
-	RTC->CR |= RTC_CR_WUCKSEL_2;		//WUCKSEL = 10x: ck_spre (usually 1 Hz) clock selected
-	RTC->CR |= RTC_CR_WUTE;				//enables RTC
+	RTC->CR &= ~RTC_CR_WUTE;				//disables the wakeup timer
+	while(!(RTC->ISR & RTC_ISR_WUTWF));		//polling WUTWF until it is set
+	RTC->WUTR 	= 	30;						//wakeup timer set to 30 seconds
+	EXTI->RTSR 	|= 	EXTI_RTSR_RT20;			//EXTI line 20 sensitive to rising edges (wakeup event)
+	RTC->CR 	|=	RTC_CR_WUCKSEL_2	|	//WUCKSEL = 10x: ck_spre (usually 1 Hz) clock selected
+					RTC_CR_WUTIE		;	//enables periodic wakeup interrupt (to exit from stop mode)
+	PWR->CR 	&= ~PWR_CR_DBP;				//disable write access to the RTC registers
+
+	NVIC_EnableIRQ(RTC_IRQn);
+	NVIC_SetPriority(RTC_IRQn, 0);
 }
