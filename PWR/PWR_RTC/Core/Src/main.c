3dfaa3d1159b57177b74191442f349dadcd43ee0
Rodrigo Celmer <rodrigocelmer@hotmail.com>
2020-06-03 17:32:09 -0300

    Fix firmware structure

--- PWR/PWR_RTC/Core/Src/main.c
+++ PWR/PWR_RTC/Core/Src/main.c
@@ -10,9 +10,12 @@
 #define TOGGLE_GREEN_LED	(GPIOA->ODR ^=GPIO_IDR_ID7_Msk)
 
 //--- GENERAL
-#define BUTTON_DEBOUNCE	50
+#define BUTTON_DEBOUNCE	50		//constant for button debounce
+#define RELEASED		0		//user button status define
+#define	PRESSED			1		//user button status define
 
-uint8_t flag_EXTI 		= 0,
+uint8_t flag_EXTI 		= 0,	//this flag is set when there's an EXTI 10 interrupt
+		USR_BT_STATUS	= 0,	//this var indicates the user button status (RELEASED or PRESSED)
 		user_bt_count	= 0;	//counter to debounce the button
 
 void MCU_Init(void);
@@ -25,53 +28,41 @@ int main(void)
 	{
 		if(USR_BT_PRESS)
 		{
-			if(user_bt_count < 250)					//limits the counting to 250
+			if(user_bt_count < 250)						//button debouncing procedure
 			{
-				user_bt_count++;					//counts up for debouncing
+				if(user_bt_count++ >= BUTTON_DEBOUNCE)	//we increment user_bt_count until it reaches BUTTON_DEBOUNCE value
+				{
+					USR_BT_STATUS = PRESSED;			//then we recognize the button is definitely pressed
+					flag_EXTI = 0;						//and clear the flag_EXTI
+				}
+				else
+				{
+					USR_BT_STATUS = RELEASED;			//or the button is not pressed
+				}
 			}
 
-			if((user_bt_count >= BUTTON_DEBOUNCE) && !(USR_BT_PRESS))
+			if(USR_BT_STATUS == PRESSED)				//when we recognized the user button was pressed
 			{
-				GREEN_LED_OFF;
-				EXTI->PR |= EXTI_PR_PIF10;
+				if((!USR_BT_PRESS))						//if it was then released, tha uC start taking action
+				{
+					GREEN_LED_OFF;						//turn the green led off
+					USR_BT_STATUS = RELEASED;			//set user button status as RELEASED
 
-				SCB->SCR = SCB_SCR_SLEEPDEEP_Msk; 	// low-power mode = stop mode
-				PWR->CR &= ~PWR_CR_PDDS;			//making sure we're entering stop mode
-				PWR->CR |= PWR_CR_CWUF;				//clears WUF after 2 system clock cycles
+					RTC->CR |= RTC_CR_WUTE;				//enables RTC - starts counting
+					asm("nop");
 
-				PWR->CR |= 	PWR_CR_LPSDSR	|		//voltage regulator in low-power mode
-							PWR_CR_ULP		;		//ultra low power mode enable
-				//LPSDSR bit must be set before the LPRUN bit is set
-				PWR->CR |=	PWR_CR_LPRUN;			//voltage regulator in low-power mode
+					__WFI();	//stop mode
 
-				//entering stop mode procedure
-//				DBGMCU->CR |= DBGMCU_CR_DBG_STOP;	//this bit needs to be set if you're going to debug this code
+					while(USR_BT_PRESS);				//holds here to wake up from stop mode
 
-//				PWR->CR |=	PWR_CR_DBP;				//enable write access to the RTC registers
-//				// --- Unlocking RTC's write protection
-//				RTC->WPR = 0xCA;
-//				RTC->WPR = 0x53;
-//				// --- RTC's unlocked
-				RTC->CR |= RTC_CR_WUTE;				//enables RTC - starts counting
-				asm("nop");
+					GREEN_LED_ON;						//turn the green led on again
 
-				__WFI();	//stop mode
-
-				while(!(GPIOA->IDR & GPIO_IDR_ID10_Msk));	//holds here (for wake from stop mode)
-
-				RTC->ISR &= ~RTC_ISR_WUTF;			//clears RTC wakeup flag
-
-				MCU_Init();
-
-				flag_EXTI = 0;
-				EXTI->PR |= EXTI_PR_PIF10;
+					RTC->ISR &= ~RTC_ISR_WUTF;			//clears RTC wakeup flag
+					flag_EXTI = 0;						//clear flag_EXTI after waking up (it is set since we pressed the button to wake up)
+					EXTI->PR |= EXTI_PR_PIF10;			//clear EXTI flag after waking up (it is set since we pressed the button to wake up)
+				}
 
 			}
-		}
-		else
-		{
-			GREEN_LED_ON;
-			user_bt_count=0;
 		}
 	}
 
@@ -80,12 +71,9 @@ int main(void)
 
 void MCU_Init(void)
 {
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;	//enable PWR clock
-
 	/*							   *
 	 * --- GPIO INITIALIZATION --- *
 	 *							   */
-
 	/* -> GPIOA
 	 * PA14 -> AF mode (SWD_IO)		| Low speed	| Push pull
 	 * PA13 -> AF mode (SWD_CK)		| Low speed	| Push pull
@@ -95,15 +83,34 @@ void MCU_Init(void)
 
 	GPIOA->MODER &=	~GPIO_MODER_MODE10_Msk;
 	GPIOA->PUPDR |= GPIO_PUPDR_PUPD10_0;
+//	EXTI->IMR	|= EXTI_IMR_IM10;
+//	EXTI->FTSR	|= EXTI_FTSR_FT10;
+//
+//	NVIC_EnableIRQ(EXTI4_15_IRQn);
+//	NVIC_SetPriority(EXTI4_15_IRQn, 0);
 
 	//Green led, debug purpose
 	GPIOA->MODER &= ~GPIO_MODER_MODE7_1;
 	GPIOA->BSRR |= GPIO_BSRR_BR_7;
+	GREEN_LED_ON;
+
+	/*									*
+	 * --- STANDBY MODE INITIALIZATION --- *
+	 *									*/
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;	//enable PWR clock
+	SCB->SCR = SCB_SCR_SLEEPDEEP_Msk; 	//low-power mode = stop mode
+	PWR->CR |= PWR_CR_PDDS;				//making sure we're entering standby mode
+	PWR->CR |= PWR_CR_CWUF		|		//clears WUF after 2 system clock cycles
+				PWR_CR_LPSDSR	|		//voltage regulator in low-power mode
+				PWR_CR_ULP		;		//ultra low power mode enable
+
+	//entering stop mode procedure
+	DBGMCU->CR |= DBGMCU_CR_DBG_STOP;	//this bit needs to be set if you're going to debug this code
 
 	/*							   *
 	 *  --- RTC INITIALIZATION --- *
 	 *							   */
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;	//enable PWR clock
 
 	PWR->CR |=	PWR_CR_DBP;					//enable write access to the RTC registers
 	// --- Unlocking RTC's write protection
